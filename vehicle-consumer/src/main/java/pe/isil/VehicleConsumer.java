9479254875543914839f12933f35f0514b358cae
Jose Ventura <ventura.jos3@gmail.com>
2020-04-28 19:18:28 -0500

    first commit for vehicle app

---
+++ vehicle-consumer/src/main/java/pe/isil/VehicleConsumer.java
@@ -0,0 +1,18 @@
+package pe.isil;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleConsumer implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleConsumer.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
